04cd800e88b1931c9a7629a785ce4e8503e3f686
Nathan Reed <nmr7393@rit.edu>
2016-04-02 02:58:30 -0400

    The tempe meme has been added.

--- trashman.py
+++ trashman.py
@@ -36,6 +36,29 @@ def groupme_callback():
 			return('ok, trash\n')
 		return('ok, no trash\n')
 
+@app.route('/tempe/', methods=['POST'])
+def tempe_callback():
+	json_body = request.get_json()
+	if json_body['group_id'] == os.environ['GROUP_ID_TEMPE'] and json_body['sender_type'] != 'bot':
+		# some degree of verification that it is sent via a groupme callback
+		# could also check for "User-Agent: GroupMeBotNotifier/1.0", but that's plenty spoofable
+
+		if json_body['sender_id'] in os.environ['TEMPE_USERS'].split(','):
+			payload = {
+				'bot_id' : os.environ['BOT_ID_TEMPE'],
+				'attachments' : [
+					{
+						'type' : 'image',
+						'url'  : 'https://i.groupme.com/108x108.png.c0162980564343f1837a787b438ae4b3',
+					},
+				],
+				# 'text'   : random.choice(quotes),
+			}
+			requests.post('https://api.groupme.com/v3/bots/post', json=payload)
+			return('ok, trash\n')
+		return('ok, no trash\n')
+
+
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 5000))
 	# app.run(host='0.0.0.0', port=port, debug=True)
